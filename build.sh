51fd0946d6d695e3b0b54db1ab02de4215ed9c07
Gabriel DiFiore <difioregabe@gmail.com>
2024-07-20 13:06:58 -0400

    refactor main variable calc class (#8)
    * refactor main variable calc class
    
    easier to read and understand
    
    * new build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if Clang is installed
+if ! command -v clang &> /dev/null; then
+    echo "Clang is not installed. Installing Clang..."
+    sudo apt-get update
+    sudo apt-get install clang
+fi
+
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
+# Create build directory
+mkdir -p build
+cd build
+
+# Generate build files using CMake
+cmake ..
+
+# Build the project
+make
